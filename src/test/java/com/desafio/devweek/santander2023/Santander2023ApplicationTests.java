0ed51cf1452c8a5b2fc18f7f4e02d0d3be4a8392
Rennan Bacili <rennanbaccili@gmail.com>
2023-09-28 19:36:19 -0300

    first commit

---
+++ src/test/java/com/desafio/devweek/santander2023/Santander2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.devweek.santander2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
